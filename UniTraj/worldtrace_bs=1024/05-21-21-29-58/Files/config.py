856ebe523c407f552e04db5f4a6e83b6d3498392
zhangsz <1548389199@qq.com>
2026-05-22 12:32:52 +0800

    添加注释，跑通block实验

---
+++ UniTraj/worldtrace_bs=1024/05-21-21-29-58/Files/config.py
@@ -0,0 +1,12 @@
+args = {
+    'data': {
+        'dataset': 'worldtrace',
+        'traj_length': 200,
+        'emb_dim': 128,
+        'num_workers': 24,
+    },
+    'training': {
+        'batch_size': 1024,
+        'n_epochs': 1000,
+    },
+}
